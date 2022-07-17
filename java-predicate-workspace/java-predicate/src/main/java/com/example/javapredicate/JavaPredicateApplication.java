17f7781715cdf3bf77d20262805fc7eca0a0a81b
J Junior <junior.jaava@gmail.com>
2022-07-17 17:43:06 +0200

    testing out predicates

---
+++ java-predicate-workspace/java-predicate/src/main/java/com/example/javapredicate/JavaPredicateApplication.java
@@ -0,0 +1,13 @@
+package com.example.javapredicate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPredicateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPredicateApplication.class, args);
+	}
+
+}
